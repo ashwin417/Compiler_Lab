091ff83b760900f2a8bdd27056d60763883cdeee
ashwin417 <ashwinr7603@gmail.com>
2023-12-19 15:51:15 +0530

    modified to read multiple emails from the file

--- Lex/Email_Checker/lex.yy.c
+++ Lex/Email_Checker/lex.yy.c
@@ -1,5 +1,5 @@
 
-#line 3 "lex.yy.c"
+#line 2 "lex.yy.c"
 
 #define  YY_INT_ALIGNED short int
 
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 2
-#define YY_END_OF_BUFFER 3
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,28 +360,28 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[17] =
+static const flex_int16_t yy_accept[16] =
     {   0,
-        0,    0,    3,    2,    2,    2,    0,    0,    0,    0,
-        1,    0,    0,    0,    1,    0
+        0,    0,    4,    2,    3,    2,    2,    2,    2,    2,
+        2,    2,    1,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    3,    1,    2,    2,    2,
-        2,    2,    2,    2,    2,    2,    2,    1,    1,    1,
-        1,    1,    1,    4,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    5,    5,    6,    5,
+        1,    1,    1,    1,    1,    1,    3,    1,    1,    1,
+        1,    1,    3,    1,    4,    5,    1,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
+        1,    1,    1,    6,    7,    7,    7,    7,    7,    7,
+        7,    7,    7,    7,    7,    7,    7,    7,    7,    7,
+        7,    7,    7,    7,    7,    7,    7,    7,    7,    7,
+        1,    1,    1,    1,    3,    1,    7,    7,    7,    7,
 
-        5,    5,    5,    5,    7,    5,    5,    5,    8,    9,
-       10,    5,    5,    5,    5,    5,    5,    5,    5,    5,
-        5,    5,    1,    1,    1,    1,    1,    1,    1,    1,
+        7,    7,    7,    7,    7,    7,    7,    7,    7,    7,
+        7,    7,    7,    7,    7,    7,    7,    7,    7,    7,
+        7,    7,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -398,37 +398,37 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[11] =
+static const YY_CHAR yy_meta[8] =
     {   0,
-        1,    2,    2,    2,    3,    3,    3,    3,    3,    3
+        1,    2,    1,    1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[20] =
+static const flex_int16_t yy_base[18] =
     {   0,
-        0,    4,   25,   26,   20,    5,   13,    0,    9,   13,
-       11,    8,    1,    2,   26,   26,   18,   20,    3
+        0,    0,   17,    0,   27,    7,    0,   13,   16,   11,
+       19,    8,    5,    4,   27,    9,    8
     } ;
 
-static const flex_int16_t yy_def[20] =
+static const flex_int16_t yy_def[18] =
     {   0,
-       17,   17,   16,   16,   18,   18,   18,   19,   18,   19,
-       18,   16,   16,   16,   16,    0,   16,   16,   16
+       15,    1,   15,   16,   15,   17,   16,   17,   16,    9,
+        9,   11,   11,   11,    0,   15,   15
     } ;
 
-static const flex_int16_t yy_nxt[37] =
+static const flex_int16_t yy_nxt[35] =
     {   0,
-        4,   16,    6,    4,    4,   10,    6,    4,    8,   15,
-       14,    9,    8,   13,    8,   12,    8,   11,    5,    5,
-        5,    7,    7,    8,   16,    3,   16,   16,   16,   16,
-       16,   16,   16,   16,   16,   16
+        4,    5,    6,    6,    6,    4,    6,    7,    8,    7,
+       14,   14,    9,    7,   13,   11,   15,   15,    9,   10,
+       10,   15,   10,   11,   15,   12,    3,   15,   15,   15,
+       15,   15,   15,   15
     } ;
 
-static const flex_int16_t yy_chk[37] =
+static const flex_int16_t yy_chk[35] =
     {   0,
-        1,    0,    1,    1,    2,   19,    2,    2,    6,   14,
-       13,    6,    9,   12,   11,   10,    7,    9,   17,   17,
-       17,   18,   18,    5,    3,   16,   16,   16,   16,   16,
-       16,   16,   16,   16,   16,   16
+        1,    1,    1,    1,    1,    1,    1,    6,   17,   16,
+       14,   13,    6,    8,   12,   10,    3,    0,    8,    9,
+        9,    0,    9,   11,    0,   11,   15,   15,   15,   15,
+       15,   15,   15,   15
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -445,11 +445,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "email.l"
-#line 2 "email.l"
+#line 1 "lex_checkmail.l"
+#line 2 "lex_checkmail.l"
     int flag = 0;
+#line 451 "lex.yy.c"
 #line 452 "lex.yy.c"
-#line 453 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -666,9 +666,9 @@ YY_DECL
 		}
 
 	{
-#line 6 "email.l"
+#line 5 "lex_checkmail.l"
 
-#line 672 "lex.yy.c"
+#line 671 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -695,13 +695,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 17 )
+				if ( yy_current_state >= 16 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 26 );
+		while ( yy_base[yy_current_state] != 27 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -727,15 +727,29 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 7 "email.l"
-{ flag=1; }
+#line 6 "lex_checkmail.l"
+{ 
+    printf("Email accepted: %s\n", yytext); 
+    flag = 1;
+}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 8 "email.l"
+#line 10 "lex_checkmail.l"
+{ 
+    flag = 0; 
+    if (!flag) {
+        printf("Email not accepted: %s\n", yytext);
+        flag = 0; 
+    }
+}
+	YY_BREAK
+case 3:
+YY_RULE_SETUP
+#line 18 "lex_checkmail.l"
 ECHO;
 	YY_BREAK
-#line 739 "lex.yy.c"
+#line 752 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1032,7 +1046,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 17 )
+			if ( yy_current_state >= 16 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1060,11 +1074,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 17 )
+		if ( yy_current_state >= 16 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 16);
+	yy_is_jam = (yy_current_state == 15);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1740,22 +1754,21 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 8 "email.l"
+#line 18 "lex_checkmail.l"
 
 
 int main(int argc, char **argv) {
     FILE *file;
     file = fopen("file.txt", "r");
     if (!file) {
-        printf("could not open the file");
+        printf("Could not open the file\n");
         exit(0);
     }
+
     yyin = file;
     yylex();
-    if (flag == 1) 
-        printf("Accepted"); 
-    else
-        printf("Not Accepted"); 
+   
+    fclose(file);
     printf("\n");
     return (0);
 }
@@ -1763,3 +1776,4 @@ int main(int argc, char **argv) {
 int yywrap() {
     return (1);
 }
+
